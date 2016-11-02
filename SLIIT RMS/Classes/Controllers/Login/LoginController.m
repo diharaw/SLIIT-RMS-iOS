c5421053455a3d799cb367c580b449472d631b44
Dihara.w <dihara.w@fidenz.com>
2016-11-02 14:10:06 +0530

    - Added temp background image. - Added login validations for Email and Password.

--- SLIIT RMS/Classes/Controllers/Login/LoginController.m
+++ SLIIT RMS/Classes/Controllers/Login/LoginController.m
@@ -25,17 +25,52 @@
     [super viewDidLoad];
 }
 
+#pragma mark - Private
+
+- (BOOL) validateEmail:(NSString*) emailString
+{
+    if([emailString length]==0){
+        return NO;
+    }
+    
+    NSString *regExPattern = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    
+    NSRegularExpression *regEx = [[NSRegularExpression alloc] initWithPattern:regExPattern options:NSRegularExpressionCaseInsensitive error:nil];
+    NSUInteger regExMatches = [regEx numberOfMatchesInString:emailString options:0 range:NSMakeRange(0, [emailString length])];
+    
+    NSLog(@"%lu", (unsigned long)regExMatches);
+    if (regExMatches == 0) {
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
 #pragma mark - UI Events
 
 - (IBAction)onLoginPressed:(id)sender
 {
-    progressHud = [[MBProgressHUD alloc] initWithView:self.view];
-    [self.view addSubview:progressHud];
-    progressHud.delegate = self;
-    [progressHud showAnimated:YES];
-    
-    [[UserSync sharedCenter] setDelegate:self];
-    [[UserSync sharedCenter] startLoginSync:self.txtEmail.text withPassword:self.txtPassword.text];
+    if([self validateEmail:self.txtEmail.text])
+    {
+        if(![self.txtPassword.text isEqualToString:@""])
+        {
+            progressHud = [[MBProgressHUD alloc] initWithView:self.view];
+            [self.view addSubview:progressHud];
+            progressHud.delegate = self;
+            [progressHud showAnimated:YES];
+            
+            [[UserSync sharedCenter] setDelegate:self];
+            [[UserSync sharedCenter] startLoginSync:self.txtEmail.text withPassword:self.txtPassword.text];
+        }
+        else
+        {
+            [TSMessage showNotificationInViewController:self title:@"Password empty!" subtitle:@"Please enter password." type:TSMessageNotificationTypeWarning duration:3];
+        }
+    }
+    else
+    {
+        [TSMessage showNotificationInViewController:self title:@"Invalid Email!" subtitle:@"Please enter a valid email address." type:TSMessageNotificationTypeWarning duration:3];
+    }
 }
 
 #pragma mark - Delegates
--- SLIIT RMS/Classes/Services/API/UserApi.m
+++ SLIIT RMS/Classes/Services/API/UserApi.m
@@ -22,6 +22,7 @@
 {
     self = [super init];
     manager =[AFHTTPSessionManager manager];
+    
     return self;
 }
 
